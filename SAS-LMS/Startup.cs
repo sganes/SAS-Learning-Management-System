100f4cff2c4fbc197aeb1c639caa75a22b6a65ea
User <User@1MZ7J32>
2017-07-19 15:34:50 +0200

    Add project files.

---
+++ SAS-LMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SAS_LMS.Startup))]
+namespace SAS_LMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
