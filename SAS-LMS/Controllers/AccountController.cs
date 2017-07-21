36259508c9d702ab4871a7bb8c2ac556029fc95f
User <User@9JZ7J32>
2017-07-21 11:04:55 +0200

    Changes to Login profile

--- SAS-LMS/Controllers/AccountController.cs
+++ SAS-LMS/Controllers/AccountController.cs
@@ -156,7 +156,7 @@ namespace SAS_LMS.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                   // await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
